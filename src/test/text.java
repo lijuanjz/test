60e806ff3670c4df65f8355cc83ba6bdf126e1d5
lijuanjz <lijuanjz@outlook.com>
2018-06-26 17:33:34 +0800

    sql

--- src/test/text.java
+++ src/test/text.java
@@ -1,10 +1,58 @@
 package test;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class text {
+	static void test(String sql){
+		Connection conn=null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("jdbc驱动载入成功？");
+			//建立数据库连接
+			String url="jdbc:oracle:thin:@localhost:1521:ORCL";
+			String uid="wanglj";
+			String passwd="brcb1234";
+			
+			conn=DriverManager.getConnection(url, uid, passwd);
+			System.out.println("jdbc连接成功");
+			
+			//构建操作数据库的工具
+			Statement stmt=conn.createStatement();
+			//sql
+			//String sql="insert into classs values('005','JAVA中级班',to_Date('2013-12-21','yyyy-mm-dd'))";
+			ResultSet rs=stmt.executeQuery(sql);
+			//System.out.println("影响行数="+an);
+			int no=0;
+			while(true){
+				boolean isNextOK=rs.next();
+				if(isNextOK==false)
+					break;
+			}
+			no++;
+			
+			rs.close();
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				conn.close();
+				System.out.println("数据库连接关闭");
+			} catch (SQLException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+		}	
+	}
 
 	public static void main(String[] args) {
-		System.out.println("eGit测试1");
-		System.out.println("第一次修改test");
+		String sql="select * from classs";
+		test(sql);
 	}
 
 }
